f85a83f8277c1279ffa71203aec1a2084ea40d98
ap514-cst <apomojumder688@gmail.com>
2026-02-14 12:31:40 +0600

    fix

--- router/search_router.js
+++ router/search_router.js
@@ -1,9 +1,10 @@
 const express=require("express");
 const { searchUser } = require("../controller/search_controller");
+const loginrequire = require("../middlware/loginrequire");
 
 const searchRouter=express.Router();
 
 
-searchRouter.get("/searchUsers", searchUser)
+searchRouter.get("/searchUsers",loginrequire ,searchUser)
 
 module.exports=searchRouter
